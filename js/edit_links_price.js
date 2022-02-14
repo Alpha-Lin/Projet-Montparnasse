2682d1cb31f75560d04ea7b6298fd7b917a344ef
Alpha-Lin <alpha_lin@protonmail.com>
2022-02-14 19:03:36 +0000

    Remove all string part after '?', especially with cdiscount URLs

--- js/edit_links_price.js
+++ js/edit_links_price.js
@@ -4,7 +4,7 @@ function add_url(url_input)
 {
     const new_url = document.createElement("div")
     new_url.className = "url_prix"
-    new_url.innerHTML = '<input name="url_prix[]" required><img src="svg/remove-button.svg" width="20" onclick="remove_url(this.parentElement)">'
+    new_url.innerHTML = '<input type="url" name="url_prix[]" onchange="correct_url(this)" required><img src="svg/remove-button.svg" width="20" onclick="remove_url(this.parentElement)">'
 
     url_input.parentElement.appendChild(new_url)
 
@@ -22,4 +22,12 @@ function remove_url(parent)
         document.getElementById('add_url_image').hidden = false
 
     nb_url_input--
+}
+
+function correct_url(url_input)
+{
+    const pos_end_url = url_input.value.indexOf("?")
+
+    if(url_input.value.indexOf("?") !== -1)
+        url_input.value = url_input.value.substring(0, pos_end_url)
 }
