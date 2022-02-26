99e4b89eb1bfd617d7e74eec2c201505889b46ad
Romain9 <romain.msiah@gmail.com>
2022-02-26 18:53:59 +0100

    un-commented line

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php     /* the front page */
-//require 'php/modules/init_bdd.php';
+require 'php/modules/init_bdd.php';
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
