4f547ec015bdd77ec85319886205dc81b8a69375
Kaputchino <andreasmulard@gmail.com>
2022-02-26 20:43:48 +0100

    minor modification of image db

--- sql/image.sql
+++ sql/image.sql
@@ -1,6 +1,6 @@
 
 CREATE TABLE IF NOT EXISTS `image` (
-  `img_name` varchar(256) NOT NULL,
-  `img_data` longblob NOT NULL,
-  PRIMARY KEY (`img_name`)
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+    `img_name` varchar(256) NOT NULL,
+    `img_data` longblob NOT NULL,
 ) 
