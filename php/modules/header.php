1874334b96c7890ec7be9d13e2627304eb1b44c3
Romain9 <romain.msiah@gmail.com>
2022-03-19 20:05:14 +0100

    début volet trie et side bar

--- php/modules/header.php
+++ php/modules/header.php
@@ -5,5 +5,20 @@ if(isset($_SESSION['id']))
     echo '<li><a href="?i=upload_announcement" style="float: right"><i class="fa fa-plus" aria-hidden="true"></i></a></li>';
 ?>
 </ul>
+<div class="volet" onclick="location.href='/js/nePasSupprimer.html';">
+    <div class="arrow"></div>
+</div>
+<div class="sidebar">
+    <div class="circle" onclick="location.href='/';">
+            <p>Click</p>
+    </div>
+    <div class="circle" onclick="location.href='/';">
+            <p>Click</p>
+    </div>
+    <div class="circle" onclick="location.href='/';">
+            <p>Click</p>
+    </div>
+</div>
 
 <div class="main-content">
+
