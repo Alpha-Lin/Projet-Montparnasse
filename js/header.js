029bade20985aa6c8d19e88958c3604a43eb7e5d
Romain9 <romain.msiah@gmail.com>
2022-04-02 16:41:24 +0200

    search button fixed, sort window finished

--- js/header.js
+++ js/header.js
@@ -1,33 +1,29 @@
-function dropDownButton()
-{
-    var btn = document.getElementById("dropdown");
-    var content = document.getElementById("dropdown-content");
-    var value = btn.getAttribute("valeur");
+var btn = document.getElementById("dropdown");
+var value = btn.getAttribute("valeur");
 
-    content.style.visibility = "visible";
-    content.style.transitionDuration = "0.7s";
-    content.style.opacity = "1";
-    content.style.zIndex = "1";
-    value = "opened";
 
+btn.addEventListener("click", function open(){
     
-    btn.onclick = function(){
-        if(value == "closed")
-        {
-            content.style.visibility = "visible";
-            content.style.transitionDuration = "0.7s";
-            content.style.opacity = "1";
-            content.style.zIndex = "1";
-            value = "opened";
-        }
-        else
-        {   
-            content.style.visibility = "hidden";
-            content.style.transitionDuration = "0.7s";
-            content.style.opacity = "0";
-            content.style.zIndex = "0";
-            value = "closed";
-        }
-        
+    var content = document.getElementById("dropdown-content");
+    
+    content.addEventListener("click", function(event){
+        event.stopPropagation();
+    });
+
+    if(value == "closed")
+    {
+        content.style.visibility = "visible";
+        content.style.transitionDuration = "0.7s";
+        content.style.opacity = "1";
+        content.style.zIndex = "1";
+        value = "opened";
     }
-}
+    else
+    {   
+        content.style.visibility = "hidden";
+        content.style.transitionDuration = "0.7s";
+        content.style.opacity = "0";
+        content.style.zIndex = "0";
+        value = "closed";
+    }
+});
