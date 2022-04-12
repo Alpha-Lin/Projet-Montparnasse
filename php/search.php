2f1961c7f385152312466ddf75d26a9a42148119
Alpha-Lin <35852129+Alpha-Lin@users.noreply.github.com>
2022-04-12 15:35:34 +0200

    limit price length

--- php/search.php
+++ php/search.php
@@ -66,7 +66,7 @@ if(!empty($produits_research))
 
                         <p class="descProduit">' . htmlspecialchars($produit['description']) . '</p>
 
-                        <p class="prixProduit">' . $prix . '€</p>
+                        <p class="prixProduit">' . number_format($prix, 2, '.', '') . '€</p>
                     </div>
                 </div>
 
