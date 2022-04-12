36bea2f3b446cf97ca28b890d5d2917719ec0766
root <alpha_lin@protonmail.com>
2022-04-12 14:46:24 +0100

    Setting up pi config

--- php/modules/init_bdd.php
+++ php/modules/init_bdd.php
@@ -5,7 +5,7 @@ $options =
 ];
 
 try{
-	$bdd = new PDO('mysql:host=localhost;dbname=stonksbay', '', ''); // user et mdp à changer par la suite
+	$bdd = new PDO('mysql:host=localhost;dbname=stonksbay', 'stonksbay', '1234'); // user et mdp à changer par la suite
 }catch(PDOException $pe){
 	die('<p>Erreur lors de l\'accès à la base de donnée : </p>');
 }
