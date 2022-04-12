9d93c18b3191b3dfb9b049a43accf133f7ef96f2
Alpha-Lin <alpha_lin@protonmail.com>
2022-04-12 13:54:44 +0100

    remove development identifies

--- php/modules/init_bdd.php
+++ php/modules/init_bdd.php
@@ -5,7 +5,7 @@ $options =
 ];
 
 try{
-	$bdd = new PDO('mysql:host=localhost;dbname=stonksbay_adrian', 'adrian', '1234'); // user et mdp à changer par la suite
+	$bdd = new PDO('mysql:host=localhost;dbname=stonksbay', '', ''); // user et mdp à changer par la suite
 }catch(PDOException $pe){
 	die('<p>Erreur lors de l\'accès à la base de donnée : </p>');
 }
