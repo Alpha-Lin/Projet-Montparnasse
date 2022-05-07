f9fb86ddf7bf0fe73512986ef65296c9b43dd0c0
Kaputchino <andreasmulard@gmail.com>
2022-05-07 17:25:33 +0200

    foreign key

--- sql/subtag.sql
+++ sql/subtag.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `subtag` (
-  `fk_idSub` int(11) DEFAULT NULL,
-  `fk_idTag` int(11) DEFAULT NULL,
-  KEY `fk_idSub` (`fk_idSub`),
-  KEY `fk_idTag` (`fk_idTag`)
+    `fk_idSub` int(11) DEFAULT NULL,
+    `fk_idTag` int(11) DEFAULT NULL,
+    FOREIGN KEY (fk_idSub) REFERENCES subscribers(idSub),
+    FOREIGN KEY (fk_idTag) REFERENCES tags(idTags)
 )
