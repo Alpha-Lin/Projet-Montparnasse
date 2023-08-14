7d24598572722dd6809ddbde81d780040783d9b2
Alpha-Lin <alpha_lin@protonmail.com>
2023-08-14 19:12:22 +0200

    MySQL friendly

--- sql/stonks-me-groups.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE stonks-me_groups ( -- représente les infos d'un groupe
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    step TINYINT UNSIGNED NOT NULL DEFAULT 0;
-    time_start DATETIME,
-    time_end DATETIME
-);
--- devise ?
